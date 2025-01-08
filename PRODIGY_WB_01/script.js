249aa5a624b7bd95ee08d77beb4d6aaaf317134a
navelikhanna <navelikhanna73@gmail.com>
2025-01-08 18:57:35 +0530

    Delete PRODIGY_WB_01 directory

--- PRODIGY_WB_01/script.js
+++
@@ -1,9 +0,0 @@
-// Navbar background change on scroll
-window.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) {
-      navbar.style.backgroundColor = "#128";
-    } else {
-      navbar.style.backgroundColor = "#00000";
-    }
-  });
